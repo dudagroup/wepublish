f3448defecbcdd54d22e18c1290702ab5dec3bca
Denis Huber <denis.huber@dudagroup.com>
2021-05-12 07:47:58 +0200

    refactor: cleanup

--- packages/editor/src/client/atoms/contentEdit/i18nWrapper.tsx
+++ packages/editor/src/client/atoms/contentEdit/i18nWrapper.tsx
@@ -1,32 +1,6 @@
 import React, {memo, ReactElement} from 'react'
 import {FormGroup, Row, Col} from 'rsuite'
 
-export interface ArticleMetadataProperty {
-  readonly key: string
-  readonly value: string
-  readonly public: boolean
-}
-
-export interface ArticleMetadata {
-  readonly evaluationBodyId: string
-  readonly evaluationBodyNumber: string
-  readonly evaluationBodyName: string
-  readonly personName: string
-  readonly academicDegree: string
-  readonly personNumber: string
-  readonly isResearchCouncilMember: string
-  readonly university: string
-  readonly website: string
-  readonly _function: string
-  readonly membershipValidFrom: string
-  readonly membershipValidUntil: string
-  readonly evaluationBodyType: string
-  readonly evaluationBodyPublishedFromDate: string
-  readonly evaluationBodyPublishedUntilDate: string
-  readonly evaluationBodySortNumber: string
-  readonly functionSortNumber: string
-}
-
 export interface I18nProps {
   readonly lane1: ReactElement | null
   readonly lane2: ReactElement | null
