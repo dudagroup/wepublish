7ef0cbaf12256725aac3d103efe0153d388367a3
GabrielAnliker <gabriel.anliker@hey.com>
2021-07-28 09:55:28 +0200

    make langswitch more visible

--- packages/editor/src/client/routes/contentEditor.tsx
+++ packages/editor/src/client/routes/contentEditor.tsx
@@ -545,10 +545,14 @@ export function ContentEditor({id, type, configs, onBack, onApply}: ArticleEdito
               width: '100%',
               maxWidth: 1140,
               position: 'sticky',
-              paddingTop: 10,
-              top: 42,
+              borderRadius: 60,
+              paddingTop: 0,
+              paddingLeft: 10,
+              paddingRight: 10,
+              top: 52,
               boxShadow: '0 5px 13px -14px #888',
-              background: '#fff',
+              background: '#f2faff',
+              border: '1px solid rgba(22, 117, 224, 0.3)',
               zIndex: 10
             }}>
             {header}
